e1a5d032c6df3860b65f43c07206ec8aac4a5705
ruben.ahlin@chasacademy.se <ruben.ahlin@chasacademy.se>
2024-01-14 22:21:46 +0100

    Quill notes card

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const time = document.querySelector('#time');
 const date = document.querySelector('#date');
+const editor = new Quill('#editor', {
+    theme: 'snow',
+    placeholder: 'Skriv något...',
+    bounds: '#editor-container',
+    style: 'border: none;',
+    modules: {
+      toolbar: [
+        [{ 'header': [1, 2, false] }],                       // Headers
+        ['bold', 'italic', 'underline', 'strike'],          // Text formatting options
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],      // Ordered and unordered lists
+      ]
+    }
+  });
 
 function updateClock() {
   const now = new Date();
